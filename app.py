c933ff8c0c899d034eea870c49b43bbffb8c7642
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-12 15:17:20 +0300

    added 404 500 page routes, bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_script import Manager
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
