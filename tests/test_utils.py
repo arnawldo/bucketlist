b1c7eb8ace75910ba62908ab7c7db0e00e31a7fd
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-14 05:27:28 +0300

    added utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,3 +11,13 @@ from app.models import Utils
 ])
 def test__util_can_validate_email__succeeds(input_email, expected_output):
     assert Utils.email_is_valid(input_email) is expected_output
+
+def test__password_hashing__succeeds():
+    password = "password"
+    hashed = Utils.hash_password(password)
+    assert password != hashed
+    check_hashed = Utils.check_hashed_password(password, hashed)
+    assert check_hashed
+    wrong_password = "wrong"
+    check_hashed = Utils.check_hashed_password(wrong_password, hashed)
+    assert check_hashed is False
